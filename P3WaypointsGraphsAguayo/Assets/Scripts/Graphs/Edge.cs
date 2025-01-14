521321a72b6f85313ab08dd55852097162460f10
janelle <145067236+jan5ll5@users.noreply.github.com>
2025-01-14 11:17:03 -0800

    fixed script
    fixed script

--- P3WaypointsGraphsAguayo/Assets/Scripts/Graphs/Edge.cs
+++ P3WaypointsGraphsAguayo/Assets/Scripts/Graphs/Edge.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Edge
-{
+public class Edge {
+
     public Node startNode;
     public Node endNode;
 
-    public Edge(Node from, Node to)
-    {
+    public Edge(Node from, Node to) {
+
         startNode = from;
         endNode = to;
     }
--- P3WaypointsGraphsAguayo/Assets/Scripts/Graphs/Node.cs
+++ P3WaypointsGraphsAguayo/Assets/Scripts/Graphs/Node.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
-{
+public class Node {
+
     public List<Edge> edgeList = new List<Edge>();
     public Node path = null;
-    GameObject id;
-   
     public float f, g, h;
     public Node cameFrom;
-    
-    public Node(GameObject i)
-    {
-        id = i;
 
+    private GameObject id;
+
+    public Node(GameObject i) {
+
+        id = i;
         path = null;
     }
 
-    public GameObject getId()
-    {
+    public GameObject getID() {
+
         return id;
     }
 }
